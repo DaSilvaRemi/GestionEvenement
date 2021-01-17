e723d88bb9acf63fc190053caa1ec1b5f0dd8c25
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2021-01-17 17:04:42 +0100

    Ajout fichiers CSV

--- src/main/java/com/cvven/modele/Files.java
+++ src/main/java/com/cvven/modele/Files.java
@@ -8,28 +8,62 @@ package com.cvven.modele;
 import java.io.File;
 
 /**
- *
+ * Classe Technique abstaite permettant de gérer des fichiers de diverses formats
+ * 
  * @author remi
  */
 public abstract class Files{
-
+    /**
+     * Le répertoire ou est stockée du fichier
+     * 
+     * ex : C:/Test/
+     */
     private String fileRepository = "/com/cvven/files";
+    
+    /**
+     * Le nom du fichier sans extension et nom de répertoire
+     */
     private String fileName;
+    
+    /**
+     * L'extension du fichier par défaut le fichier est au format txt
+     */
     private String fileExtension = ".txt";
-    private String fullFileName;
+    
+    /**
+     * URI/Emplacement du fichier contenant les 3 paramètre :
+     *  -fileRepository
+     *  -fileName
+     *  -fileExtension
+     * 
+     * ex : C:/Test/test2.txt
+     */
+    private String fileURI;
+    
+    /**
+     * La fichier en format objet
+     */
     private File file;
     
     /**
+     * Constructeur par défaut nécessitant uniquement le nom du fichier
      * 
+     * Le répertoire et l'extension du fichier seront déclarée avec des valeurs par défaut
+     * 
+     * @see fileRepository
+     * @see fileExtension
      * @param fileName 
      */
     protected Files(String fileName) {
         this.fileName = fileName;
-        this.fullFileName = this.fileRepository + this.fileName + this.fileExtension;
-        this.file = new File(this.fullFileName);
+        this.fileURI = this.fileRepository + this.fileName + this.fileExtension;
+        this.file = new File(this.fileURI);
     }
     
     /**
+     * Constructeur par défaut nécessitant uniquement deux paramètre
+     * 
+     * L'extension sera mis automatiquement sur .txt
      * 
      * @param fileName
      * @param fileRepository 
@@ -37,11 +71,12 @@ public abstract class Files{
     protected Files(String fileName, String fileRepository) {
         this.fileName = fileName;
         this.fileRepository = fileRepository;
-        this.fullFileName = this.fileRepository + this.fileName + this.fileExtension;
-        this.file = new File(this.fullFileName);
+        this.fileURI = this.fileRepository + this.fileName + this.fileExtension;
+        this.file = new File(this.fileURI);
     }
     
     /**
+     * Constructeur par défaut
      * 
      * @param fileName
      * @param fileRepository 
@@ -51,82 +86,86 @@ public abstract class Files{
         this.fileName = fileName;
         this.fileRepository = fileRepository;
         this.fileExtension = fileExtension;
-        this.fullFileName = this.fileRepository + this.fileName + this.fileExtension;
-        this.file = new File(this.fullFileName);
+        this.fileURI = this.fileRepository + this.fileName + this.fileExtension;
+        this.file = new File(this.fileURI);
     }
 
     /**
-     * @return the fileRepository
+     * @return le répertoire du fichuer
      */
     public String getFileRepository() {
         return fileRepository;
     }
 
     /**
-     * @return the fileName
+     * @return le nom du fichier
      */
     public String getFileName() {
         return fileName;
     }
 
     /**
-     * @return the extension
+     * @return l'extension
      */
     public String getFileExtension() {
         return fileExtension;
     }
 
     /**
-     * @return the fullFileName
+     * @return l'URI du fichier
      */
-    public String getFullFileName() {
-        return fullFileName;
+    public String getFileURI() {
+        return fileURI;
     }
 
     /**
-     * @param fileRepository the fileRepository to set
+     * @param fileRepository le répertoire ou est stockée le fichier
      */
     public void setFileRepository(String fileRepository) {
         this.fileRepository = fileRepository;
     }
 
     /**
-     * @param fileName the fileName to set
+     * @param fileName le nom du fichier
      */
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
 
     /**
-     * @param fileExtension the extension to set
+     * @param fileExtension l'extension du fichier
      */
     public void setFileExtension(String fileExtension) {
         this.fileExtension = fileExtension;
     }
 
     /**
-     * @param fullFileName the fullFileName to set
+     * @param fileURI l'URI du fichier
      */
-    public void setFullFileName(String fullFileName) {
-        this.fullFileName = fullFileName;
+    public void setFileURI(String fileURI) {
+        this.fileURI = fileURI;
     }
     
     /**
-     * @return the file
+     * @return le fichier
      */
     public File getFile() {
         return file;
     }
     
     /**
+     * Remplace le fichier courant aen prenant comme chemin relatif par défault l'URI du fichier indiquée dans l'objet
      * 
+     * @see fileURI
      */
     public void setFile(){
-        this.file = new File(this.fullFileName);
+        this.file = new File(this.fileURI);
     }
 
     /**
-     * @param file the file to set
+     * Remplace le fichier courant
+     * 
+     * @param file le fichier
      */
     public void setFile(File file) {
         this.file = file;
