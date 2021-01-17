233f7e516453765fd216fc0477146305e66e1b9f
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2021-01-17 17:02:13 +0100

    Ajout fichiers CSV

--- src/main/java/com/cvven/modele/CSVFileRead.java
+++ src/main/java/com/cvven/modele/CSVFileRead.java
@@ -10,10 +10,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 /**
- *
+ * Classe technique permettant de lire les fichiers CSV
+ * 
  * @author remi
  */
 public class CSVFileRead extends Files{
@@ -21,71 +20,69 @@ public class CSVFileRead extends Files{
     private ArrayList<String[]> data;
     
     /**
-     * @return the fileRead
+     * Constructeur par défaut nécessitant uniquement le nom du fichier
+     * 
+     * @param fileName 
+     */
+    public CSVFileRead(String fileName) {
+        super(fileName);
+        super.setFileExtension(".csv");
+    }
+    
+    /**
+     * Constructeur par défaut nécessitant le nom du fichier et du répertoire
+     * 
+     * @param fileName
+     * @param fileRepository 
+     */    
+    public CSVFileRead(String fileName, String fileRepository) {
+        super(fileName, fileRepository, ".csv");
+    }
+    
+    /**
+     * @return le fichier à lire
      */
     public CSVReader getFileRead() {
         return fileRead;
     }
 
     /**
-     * @return the data
+     * @return les données
      */
     public ArrayList<String[]> getData() {
         return data;
     }
 
     /**
-     * @param fileRead the fileRead to set
+     * @param fileRead le fichier à lire
      */
     public void setFileRead(CSVReader fileRead) {
         this.fileRead = fileRead;
     }
 
     /**
-     * @param data the data to set
+     * Ouvre le fichier
+     * 
+     * @throws FileNotFoundException 
      */
-    public void setData(ArrayList<String[]> data) {
-        this.data = data;
-    }
-    
-    /**
-     * 
-     * @param fileName 
-     */
-    public CSVFileRead(String fileName) {
-        super(fileName);
-    }
-    
-    /**
-     * 
-     * @param fileName
-     * @param fileRepository 
-     */    
-    public CSVFileRead(String fileName, String fileRepository) {
-        super(fileName, fileRepository);
-    }
-    
-    /**
-     * 
-     * @param fileName
-     * @param fileRepository 
-     * @param fileExtension 
-     */    
-    public CSVFileRead(String fileName, String fileRepository, String fileExtension) {
-        super(fileName, fileRepository);
-    }
-    
     public void openFile() throws FileNotFoundException{
         this.setFileRead(new CSVReader(new FileReader(super.getFile())));
     }
     
+    /**
+     * Ferme le fichier
+     * 
+     * @throws IOException 
+     */
     public void closeFile() throws IOException{
         this.getFileRead().close();
     }
+    
      /**
+      * Lit le fichier
       * 
       */
-    public void readFile(){
+    public void readFileWithoutControl(){
         try {
             this.openFile();
             String line[];
@@ -100,11 +97,34 @@ public class CSVFileRead extends Files{
         }
     }
     
+    public void readFileWithControl(){
+    
+    }
+    
+    private void readFile(int index){
+        try {
+            this.openFile();
+            String line[];
+            while((line = this.getFileRead().readNext()) != null){
+                if(index == 1){
+                    this.data.add(line);
+                }else{
+                    this.data.add(line);
+                }
+            }
+            this.closeFile();
+        } catch (FileNotFoundException ex) {
+            DialogTools.openMessageDialog(ex.getMessage(), "Erreur Fichier !", DialogTools.WARNING_MESSAGE);
+        } catch (IOException ex) {
+            DialogTools.openMessageDialog(ex.getMessage(), "Erreur Fichier !", DialogTools.WARNING_MESSAGE);
+        }
+    }
+    
     /**
      * 
      * @return 
      */
-    public boolean controlData(){
+    private boolean controlData(){
         return true;
     }
     
